9a09399ea2c0263f5ba210bd5b4133d9cf33eeba
Raul Herrera Macias <facelessdivine@gmail.com>
2022-11-24 22:10:05 -0600

    animator controller of the mole

--- Assets/Scripts/moleBehavior.cs
+++ Assets/Scripts/moleBehavior.cs
@@ -6,14 +6,48 @@ public class moleBehavior : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    private Transform target;
+    GameSession gameSession;
+    [SerializeField]
+    const int attckD = 8;
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        gameSession = FindObjectOfType<GameSession>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!gameSession.GetGameRunning())
+        {
+            return;
+        }
+        SetAnimatorStatesFalse();
+        switch (Vector2.Distance(transform.position, target.position))
+        {
+            case < attckD and > attckD - 2:
+                myAnimator.SetBool("isLeaningOut1", true);
+                break;
+            case < attckD - 4 and > attckD - 6:
+                myAnimator.SetBool("isLeaningOut2", true);
+                break;
+            case < attckD - 6 and > attckD - 8:
+                myAnimator.SetBool("isLeaningOut3", true);
+                break;
+            case < attckD - 8:
+                myAnimator.SetBool("isAttacking", true);
+                break;
+            default:
+                SetAnimatorStatesFalse();
+                break;
+        }
+    }
+    void SetAnimatorStatesFalse()
+    {
+        myAnimator.SetBool("isLeaningOut1", false);
+        myAnimator.SetBool("isLeaningOut2", false);
+        myAnimator.SetBool("isLeaningOut3", false);
+        myAnimator.SetBool("isAttacking", false);
     }
 }
